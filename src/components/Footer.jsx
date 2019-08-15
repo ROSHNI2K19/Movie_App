0027ed6f0b57762807f0e18afe2ed0b9e07f9ba2
ROSHNI2K19 <letters2roshni@gmail.com>
2019-08-15 10:55:43 +0200

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p>Made with &hearts; in Frankfurt</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer
